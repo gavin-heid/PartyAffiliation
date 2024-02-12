f324bb01667a5c056d566291c65a6713a54b3db3
Lab_04_Part1_Finished <gavin.heid04@gmail.com>
2024-02-11 21:24:31 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,35 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         String affiliationChoice = "";
         String menu = "D - democrat, R - republican, I - independent"; //DRI
         //display the menu
         //input affiliationChoice
-        affiliationChoice = "e";
-        switch (affiliationChoice)
+        affiliationChoice = " ";
+        System.out.println("What is your party affiliation?");
+        if (in.hasNext())
         {
-            case "D":
-            case "d":
-                System.out.println("You get a democratic donkey");
-                break;
-            case "R":
-            case "r":
-                System.out.println("You get a republican elephant");
-                break;
-            case "I":
-            case "i":
-                System.out.println("You are independent");
-                break;
-            default:
-                System.out.println("You chose a party other than democrat, republican, or independent!");
+            affiliationChoice = in.next();
+                    switch (affiliationChoice)
+                    {
+                        case "D":
+                        case "d":
+                            System.out.println("You get a democratic donkey");
+                            break;
+                        case "R":
+                        case "r":
+                            System.out.println("You get a republican elephant");
+                            break;
+                        case "I":
+                        case "i":
+                            System.out.println("You are independent");
+                            break;
+                        default:
+                            System.out.println("You chose a party other than democrat, republican, or independent!");
+                    }
         }
+
 
     }
 }
